38ca999aca478f9a65e43155d7f92d64791ddf89
Matheus Soares <matheus.sousoa.dev@gmail.com>
2023-06-12 17:35:10 -0300

    default

--- src/main/java/br/com/api/ghfluzao/security/AuthenticationFilter.java
+++ src/main/java/br/com/api/ghfluzao/security/AuthenticationFilter.java
@@ -18,7 +18,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 
-
+@Component
 public class AuthenticationFilter extends OncePerRequestFilter{
 
     @Autowired
