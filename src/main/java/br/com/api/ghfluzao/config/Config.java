24901abc2523e03d6c4b24adbf9773c1a46bb6a9
Matheus Soares <matheus.sousoa.dev@gmail.com>
2023-06-13 02:04:31 -0300

    cors

--- src/main/java/br/com/api/ghfluzao/config/Config.java
+++ src/main/java/br/com/api/ghfluzao/config/Config.java
@@ -2,19 +2,15 @@ package br.com.api.ghfluzao.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class Config implements WebMvcConfigurer {
+public class Config implements WebMvcConfigurer{
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // Altere para o endereço do seu aplicativo React
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**");
     }
 }
+
 
