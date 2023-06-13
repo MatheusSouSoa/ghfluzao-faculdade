2fdba5a6fd3322793ab9d12641c3233cb3961ef9
Matheus Soares <matheus.sousoa.dev@gmail.com>
2023-06-13 02:29:11 -0300

    cors

--- src/main/java/br/com/api/ghfluzao/config/Config.java
+++ src/main/java/br/com/api/ghfluzao/config/Config.java
@@ -2,35 +2,25 @@ package br.com.api.ghfluzao.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Config implements WebMvcConfigurer{
-    
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
+public class Config {
 
     @Bean
-    public CorsFilter corsFilter() {
-    final CorsConfiguration config = new CorsConfiguration();
-    config.addAllowedHeader("*");
-    config.addAllowedMethod("GET");
-    config.addAllowedMethod("POST");
-    config.addAllowedMethod("PUT");
-    config.addAllowedMethod("OPTIONS");
-    config.setMaxAge(3600L);
-
-    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", config);
-
-    return new CorsFilter(source);
-}
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                    .addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedHeaders("*")
+                    .allowedMethods("*");
+            }
+        };
+    }
 }
 
 
