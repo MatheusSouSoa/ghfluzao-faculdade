b5b8e394a701fd7b16ad8e8203eaaf10a05c98d3
Matheus Soares <matheus.sousoa.dev@gmail.com>
2023-06-13 02:20:33 -0300

    cors

--- src/main/java/br/com/api/ghfluzao/config/Config.java
+++ src/main/java/br/com/api/ghfluzao/config/Config.java
@@ -1,6 +1,10 @@
 package br.com.api.ghfluzao.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +15,22 @@ public class Config implements WebMvcConfigurer{
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+    final CorsConfiguration config = new CorsConfiguration();
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("GET");
+    config.addAllowedMethod("POST");
+    config.addAllowedMethod("PUT");
+    config.addAllowedMethod("OPTIONS");
+    config.setMaxAge(3600L);
+
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsFilter(source);
+}
 }
 
 
